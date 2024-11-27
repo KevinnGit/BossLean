c4e5e9c271e4e649c54ca696b634c9e129b40e0c
K <100544674+KevinnGit@users.noreply.github.com>
2024-11-27 10:14:07 +0800

    mo gana na ang sql

--- Pet-Shop-Management/src/main/java/module-info.java
+++ Pet-Shop-Management/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module finalprjct.petshopmanagementsystem {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
 
     opens finalprjct.petshopmanagementsystem to javafx.fxml;
